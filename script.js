785cfdc820860a20959e8534ad2430cdea3ae103
Sanoj Kumar <sanojsanoj638@gmail.com>
2022-04-19 23:55:45 +0530

    Calculator using HTML, CSS and JAvaScript

---
+++ script.js
@@ -0,0 +1,25 @@
+var screen = document.querySelector("#screen");
+buttons = document.querySelectorAll("button");
+ var screenValue = '';
+ for(item of buttons){
+     item.addEventListener('click', (e)=>{
+         buttonText = e.target.innerText;
+        //  console.log("button text is " + buttonText)
+         if(buttonText == 'X'){
+             buttonText = '*';
+             screenValue += buttonText;
+             screen.value = screenValue;
+         }
+         else if(buttonText == 'C'){
+             screenValue = "";
+             screen.value = screenValue;
+         }
+          else if(buttonText == '='){
+             screen.value = eval(screenValue);
+         }
+         else{
+             screenValue += buttonText;
+             screen.value = screenValue;
+         }
+     })
+ }
